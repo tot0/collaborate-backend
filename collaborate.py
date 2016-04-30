067ae08fe08430a9b027e4c81cb410eb96c54d5e
Chybby <achybby@gmail.com>
2016-04-30 13:33:49 +1000

    added gentrification endpoint

--- collaborate.py
+++ collaborate.py
@@ -1,3 +1,9 @@
+from __future__ import (
+    print_function,
+    absolute_import,
+)
+
+import json
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -6,14 +12,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/collaborate.db'
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
+@app.route("/get_gentrified")
+def get_gentrified():
+    return json.dumps({
+        "memes": "spicy"
+    })
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
-
-    def __repr__(self):
-        return '<User %r>' % self.username
+if __name__ == '__main__':
+    app.run()
